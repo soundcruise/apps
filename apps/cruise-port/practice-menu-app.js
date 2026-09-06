737d238458754fa54b101e272120ce101102ee52
Sound Cruise <soundcruise.inc@gmail.com>
2026-09-06 16:27:14 +0900

    Cruise PortをVer 0.5.0へ更新

--- apps/cruise-port/practice-menu-app.js
+++ apps/cruise-port/practice-menu-app.js
@@ -38,7 +38,7 @@ import { initMetronome } from './metronome-app.js';
 import {
     applyVersionDisplay,
     reloadAppWithCacheBust
-} from './app-version.js?v=1.4.0';
+} from './app-version.js?v=1.5.0';
 import { initTuner } from './tuner-app.js?v=1.1.8';
 
 const elements = {
